6f1ee69353a089e00a75a9b0666514827806beaa
Soubhik Kumar Mitra <59209034+x612skm@users.noreply.github.com>
2022-03-15 16:52:22 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (23.45%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.cpp
+++ 278-first-bad-version/278-first-bad-version.cpp
@@ -5,12 +5,12 @@ class Solution {
 public:
     int firstBadVersion(int n) {
         int start=0, end=n;
-        //cout<<end-start<<end;
         while(end-start>1){
             int mid=start+(end-start)/2;
-            /** mid = (start+end)) / 2; **/
-            if(isBadVersion(mid))  end=mid;
-            else  start=mid;
+            if(isBadVersion(mid))  
+                end=mid;
+            else  
+                start=mid;
         }
         return end;
     }
