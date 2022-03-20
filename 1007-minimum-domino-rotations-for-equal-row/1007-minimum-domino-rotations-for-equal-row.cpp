cc2eff62433f2771d2959c44ab3027910ff223e3
Soubhik Kumar Mitra <59209034+x612skm@users.noreply.github.com>
2022-03-20 18:07:29 +0530

    Time: 108 ms (95.45%), Space: 111.7 MB (29.27%) - LeetHub

--- 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.cpp
+++ 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.cpp
@@ -4,13 +4,15 @@ public:
         vector<int> countA(7), countB(7), same(7);
         int n = tops.size();
         
+        //mathematical for the calculation approach
         for(int i=0; i<n ; i++){
-            countA[tops[i]]++;
+            countA[tops[i]]++; //if it is there then put it and ++ if not then put it inc to 1
             countB[bottoms[i]]++;
             
             if(tops[i] == bottoms[i])
                 same[tops[i]]++;
         }
+        
         
         for(int i=1; i < 7; i++){
             if(countA[i] + countB[i] - same[i] == n){
