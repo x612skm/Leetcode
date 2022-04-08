2895fc9d4ce106019ae56609db94fb6380893dfc
Soubhik Kumar Mitra <59209034+x612skm@users.noreply.github.com>
2022-04-09 01:05:48 +0530

    Time: 5 ms (14.50%), Space: 6.5 MB (71.55%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -1,12 +1,16 @@
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
+        //taking the r as the vector in vector for the ans
         vector<vector<int>>r(numRows);
-        
+        //traversing for the numvers till the number
         for(int i=0; i<numRows; i++){
+            //resize all the ith elements to the next i+1
             r[i].resize(i+1);
+            //first and second element
             r[i][0] = r[i][i] = 1;
             
+            //traverse for the rest of the rows
             for(int j=1; j<i; j++){
                 r[i][j] = r[i-1][j-1] + r[i-1][j]; 
             }
