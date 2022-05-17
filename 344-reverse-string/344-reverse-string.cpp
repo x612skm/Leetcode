18dafa848996dbec62a9b04a65e5bc23f434e992
Soubhik Kumar Mitra <59209034+x612skm@users.noreply.github.com>
2022-05-18 00:00:09 +0530

    Time: 11 ms (99.65%), Space: 23.3 MB (42.15%) - LeetHub

--- 344-reverse-string/344-reverse-string.cpp
+++ 344-reverse-string/344-reverse-string.cpp
@@ -3,7 +3,15 @@ public:
     void reverseString(vector<char>& s) {
         int start = 0; int end = s.size()-1;
         while(start < end){
-            swap(s[start++], s[end--]);
+            //swap(s[start++], s[end--]);
+            //replacing the swap without using a temporary variable
+            
+            s[start] ^= s[end];
+            s[end] ^= s[start];
+            s[start] ^= s[end];
+            
+            ++start;
+            --end;
         }
     }
 };
