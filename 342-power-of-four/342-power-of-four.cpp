e06b0c8f762c385b1920690f8a63ef30885bb55e
Soubhik Kumar Mitra <59209034+x612skm@users.noreply.github.com>
2022-07-19 23:32:50 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (24.42%) - LeetHub

--- 342-power-of-four/342-power-of-four.cpp
+++ 342-power-of-four/342-power-of-four.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
     bool isPowerOfFour(int n) {
+        //base case for the following
         if(n<1) return false;
         if(n==1) return true;
-        
+        //recursive code
         return n%4==0 && isPowerOfFour(n/4);
     }
 };
