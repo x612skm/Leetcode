5c983fb8933205c881727797a6170f88dbc2d79f
Soubhik Kumar Mitra <59209034+x612skm@users.noreply.github.com>
2022-07-29 18:21:24 +0530

    Time: 3 ms (25.00%), Space: 6 MB (25.00%) - LeetHub

--- 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.cpp
+++ 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     char repeatedCharacter(string s) {
         unordered_map<char,int>mp;
-        char ans;
+        char ans; //to store the ans
         for(auto it:s){
             if(mp.find(it) != mp.end()){
                 ans=it; //the char which comes first two times will be the ans
