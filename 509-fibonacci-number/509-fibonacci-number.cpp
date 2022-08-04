cc49c9f9d1088b80d8302503dcbbc4b67a93d5ff
Soubhik Kumar Mitra <59209034+x612skm@users.noreply.github.com>
2022-08-04 23:57:29 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (78.85%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -2,6 +2,18 @@ class Solution {
 public:
     int fib(int n) {
         if(n < 2) return n;
-        return fib(n-1) + fib(n-2);
+        
+        int dp[n+1];
+
+        //storing the data 
+        //memozied version of the dp
+        dp[0] = 0;
+        dp[1] = 1;
+        
+        //we will start with the next tree
+        for(int i=2; i<=n; ++i){
+            dp[i] = dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
 };
