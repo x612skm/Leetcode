4156d46cb03cf94e67844f162be900dda8a54290
Soubhik Kumar Mitra <59209034+x612skm@users.noreply.github.com>
2022-08-06 23:42:53 +0530

    Time: 53 ms (80.00%), Space: 17.8 MB (20.00%) - LeetHub

---
+++ 2363-merge-similar-items/2363-merge-similar-items.cpp
@@ -0,0 +1,25 @@
+
+class Solution {
+public:
+    vector<vector<int>> mergeSimilarItems(vector<vector<int>>& items1, vector<vector<int>>& items2) {
+        //this question has a clear base regarding mapping their values with the wieght sum
+        //so here we do the operaition using a hashmap
+        //key value pairing
+        
+        map<int,int>mp; //unordered_map does not sort the things that why it is req.
+        //inserting into the map
+        for(auto i : items1) 
+            mp[i[0]] += i[1];
+        
+        for(auto i : items2) 
+            mp[i[0]] += i[1];
+        
+        //to store it from the map we need a vector
+        vector<vector<int>> v;
+        
+        for(auto i:mp)
+            v.push_back({i.first, i.second}); //{} brackets are used for the 2D
+            
+        return v;
+    }
+};
