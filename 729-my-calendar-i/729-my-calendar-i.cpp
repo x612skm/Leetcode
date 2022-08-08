6eb233b991ce3a89db57a5a236c0ab46e0f95167
Soubhik Kumar Mitra <59209034+x612skm@users.noreply.github.com>
2022-08-08 17:11:07 +0530

    Time: 116 ms (81.92%), Space: 39.1 MB (29.52%) - LeetHub

---
+++ 729-my-calendar-i/729-my-calendar-i.cpp
@@ -0,0 +1,27 @@
+class MyCalendar {
+public:
+    //we created a ordered map here 
+    map<int,int>mp;
+    MyCalendar() {
+        
+    }
+    bool book(int start, int end) {
+        auto it = mp.upper_bound(start);
+        //we check the overflow
+        if(it != mp.end()){
+            if(it->first < end) return false;
+        }
+        
+        if(it != mp.begin()){
+            if(prev(it)->second > start) return false;
+        }
+        mp[start] = end;
+        return true;
+    }
+};
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * MyCalendar* obj = new MyCalendar();
+ * bool param_1 = obj->book(start,end);
+ */
