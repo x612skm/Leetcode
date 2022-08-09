60d9e42988d6a61b96d1e6c6acacc6403a2ab032
Soubhik Kumar Mitra <59209034+x612skm@users.noreply.github.com>
2022-08-10 02:16:54 +0530

    Time: 145 ms (83.35%), Space: 9.6 MB (86.45%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -2,40 +2,23 @@ class Solution {
 public:
     bool canPartition(vector<int>& nums) {
         int sum = 0;
-        //taing all the numbers inside the sum
-        for(auto i : nums)
-            sum += i;
+        for(auto it : nums)
+            sum += it;
         
-        if(sum&1) return false;
+        if(sum % 2)
+            return false;
         
+        sum /= 2;
         
-        int target = sum/2;
+        vector<bool>dp(sum+1,false);
         
-        //making the matrix
-        vector<vector<bool>> dp(nums.size()+1, vector<bool>(target+1));
+        dp[0] = true;
         
-        for(int i=0; i<=nums.size();i++){
-            for(int j=0; j<=target;j++){
-                //taking the base case
-                if(i==0 && j==0)
-                    dp[i][j] = true;
-                
-                else if(j==0 && i!=0)
-                    dp[i][j] = true;
-                
-                else if(i==0 && j!=0)
-                    dp[i][j] = false;
-                
-                //if inputs of the element is less than or equal to the weight
-                //formula for the knapsack for pick it or not pick it
-                else if(nums[i-1] <= j){
-                    dp[i][j] = dp[i-1][j-nums[i-1]] || dp[i-1][j];
-                }
-                //simply execute the item
-                else
-                    dp[i][j] = dp[i-1][j];
+        for(auto it:nums){
+            for(int i = sum; i>= it; i--){
+                dp[i] = dp[i] || dp[i-it];
             }
         }
-        return dp[nums.size()][target];
+        return dp[sum];
     }
 };
