03be5e936d9c107377fd348b0b68cbdf131fb323
Soubhik Kumar Mitra <59209034+x612skm@users.noreply.github.com>
2022-08-10 14:55:32 +0530

    Time: 26 ms (66.78%), Space: 11.8 MB (71.06%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.cpp
+++ 518-coin-change-2/518-coin-change-2.cpp
@@ -1,25 +1,31 @@
 class Solution {
 public:
-    int change(int amount, vector<int>& coins) {
-        int n = coins.size();
-        int dp[n+1][amount+1];
+    int findLow(vector<int>& coins, int arrSize, int amount){
+        int dp[arrSize + 1][amount+1];
+        //initilise the matrix
+        for(int i=0; i< arrSize+ 1; i++){
+            for(int j=0; j< amount + 1; j++){
+                if(i==0)
+                    dp[i][j] = 0;
+                if(j==0)
+                    dp[i][j] = 1;
+                    // to avoid the overflow
+            }
+        }
         
-        for (int i = 0; i < n + 1; ++i) {
-		for (int j = 0; j < amount + 1; ++j) {
-			if (i == 0) 
-                dp[i][j] = 0;
-			if (j == 0) 
-                dp[i][j] = 1;
-		}
-	}
-        for(int i=1; i<=n; i++){
-            for(int j=1; j<=amount; j++){
+        for(int i=1; i<arrSize+1; i++){
+            for(int j=1; j<amount+1; j++){
                 if(coins[i-1] <= j)
-                    dp[i][j] = dp[i-1][j] + dp[i][j-coins[i-1]];
+                    dp[i][j] =  dp[i-1][j] + dp[i][j-coins[i-1]] ;
                 else
                     dp[i][j] = dp[i-1][j];
             }
         }
-        return dp[n][amount];
+        return dp[arrSize][amount];
+        
+    }
+    int change(int amount, vector<int>& coins) {
+        int n = coins.size();
+        return findLow(coins,n,amount);
     }
 };
