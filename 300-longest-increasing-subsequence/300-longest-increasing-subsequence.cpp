9769fc0fc413ae9283a36662a518eb66e19452c8
Soubhik Kumar Mitra <59209034+x612skm@users.noreply.github.com>
2022-08-12 18:22:02 +0530

    Time: 393 ms (52.67%), Space: 10.5 MB (69.86%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,15 +1,23 @@
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        int n = size(nums);
-        vector<int>dp(n,1);
+        //there is a pattern to solve the lis
+        int n = nums.size();
+        vector<int>lis(n+1, 1);
         
-        for(int i=0;i<n; ++i){
+        //we make two loops to read the array
+        for(int i=0; i<n; i++){
             for(int j=0; j<i; j++){
-                if(nums[i] > nums[j] and dp[i] < dp[j] + 1)
-                    dp[i] = dp[j] + 1;
+                if(nums[i] > nums[j] and 1+lis[j] > lis[i])
+                    lis[i] = 1 + lis[j];
             }
         }
-        return *max_element(dp.begin(), dp.end());
+        
+        int ans = 0;
+        //to stroe the answer
+        for(int i=0; i<n; i++){
+            ans = max(ans, lis[i]); //this lis[i] is known to have the lis length
+        }
+        return ans;
     }
 };
