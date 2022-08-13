0902eabbda864e92b1200077068c1b889431c4b6
Soubhik Kumar Mitra <59209034+x612skm@users.noreply.github.com>
2022-08-14 00:47:20 +0530

    Time: 1055 ms (5.02%), Space: 77.6 MB (37.80%) - LeetHub

--- 354-russian-doll-envelopes/354-russian-doll-envelopes.cpp
+++ 354-russian-doll-envelopes/354-russian-doll-envelopes.cpp
@@ -1,28 +1,24 @@
 class Solution {
 public:
-    static bool cmp(vector<int>& a, vector<int>& b){
-        if(a[0]==b[0]) return a[1] > b[1];
+    static bool comp(vector<int>&a, vector<int>&b){
+        if(a[0] == b[0])
+            return a[1] > b[1];
         return a[0] < b[0];
     }
-    
-int maxEnvelopes(vector<vector<int>>& envelopes) {
-        int N = envelopes.size();
-        vector<int> candidates;
-        sort(envelopes.begin(), envelopes.end(), cmp);
-        for (int i = 0; i < N; i++) {
-            int lo = 0, hi = candidates.size() - 1;
-            while (lo <= hi) {
-                int mid = lo + (hi - lo)/2;
-                if (envelopes[i][1] > envelopes[candidates[mid]][1])
-                    lo = mid + 1;
-                else
-                    hi = mid - 1;
-            }
-            if (lo == candidates.size())
-                candidates.push_back(i);
+    int maxEnvelopes(vector<vector<int>>& envelopes) {
+        // we cannot have matrix operations as .first and .second
+        int n = envelopes.size();
+        vector<int>lis;
+        sort(envelopes.begin(), envelopes.end(),comp);
+        for(int i=0; i<n; i++){
+            int ele = envelopes[i][1];
+            int index = lower_bound(lis.begin(), lis.end(), ele)-lis.begin();
+            cout<<index;
+            if(index >= lis.size())
+                lis.push_back(ele);
             else
-                candidates[lo] = i;
+                lis[index] = ele;
         }
-        return candidates.size();
+        return lis.size();
     }
 };
