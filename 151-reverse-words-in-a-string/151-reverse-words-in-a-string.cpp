f1df5919bc30a4b64db1459f9bc320c371324179
Soubhik Kumar Mitra <59209034+x612skm@users.noreply.github.com>
2022-08-14 16:50:01 +0530

    Time: 23 ms (19.26%), Space: 20.9 MB (18.78%) - LeetHub

--- 151-reverse-words-in-a-string/151-reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/151-reverse-words-in-a-string.cpp
@@ -2,10 +2,14 @@ class Solution {
 public:
     string reverseWords(string s) {
         stringstream ss(s);
+        
         string result , word;
+        cout<<result;
         while(ss>>word){
             if(word != " ")
+                cout<<word;
                 result = word + " " + result;
+                
         }
         result.pop_back();
        
