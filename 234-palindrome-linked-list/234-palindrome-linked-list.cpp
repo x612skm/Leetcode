2eb00de90418017e9ddfde616c88996cb8513598
Soubhik Kumar Mitra <59209034+x612skm@users.noreply.github.com>
2022-08-25 01:00:38 +0530

    Time: 245 ms (93.54%), Space: 114.2 MB (74.71%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/234-palindrome-linked-list.cpp
@@ -13,21 +13,25 @@ public:
     bool isPalindrome(ListNode* head) {
         ListNode* slow=head; ListNode* fast = head, *prev, *temp;
         if(!head and !head->next) return true;
+        //find the middle using the tortoise and hare method
         while(fast and fast->next)
         {
             slow = slow->next;
             fast = fast->next->next;
         }
+        //initilise the middle ekement and after that
         prev= slow;
         slow = slow->next;
         prev->next = NULL;
-        
+        //reverse from the slow and to the end
         while(slow){
             temp = slow->next;
             slow->next = prev;
             prev = slow;
             slow = temp;
         }
+        //initlise fast to the start and compare the vals
+        //equals tends to the true
         fast = head;
         slow = prev;
         while(slow){
