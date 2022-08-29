7356303cd310905af1bb28d17cef5c321ccba852
Soubhik Kumar Mitra <59209034+x612skm@users.noreply.github.com>
2022-08-29 17:06:32 +0530

    Time: 40 ms, Memory: 20 MB - LeetHub

---
+++ single-number/single-number.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        vector<int> output;
+        int n = nums.size();
+        //finding the minimum frequency
+        unordered_map<int,int> mp;
+        for(int i=0; i<n; i++)
+            mp[nums[i]]++;
+        
+        int count = n+1; int res = -1;
+        for(auto it:mp){
+            if(it.second < count){
+                res = it.first;
+                count = it.second;
+            }
+        }
+        return res;
+    }
+};
