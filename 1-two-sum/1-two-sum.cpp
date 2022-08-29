d6d524980f6f8e21df00c9643da08454170ab650
Soubhik Kumar Mitra <59209034+x612skm@users.noreply.github.com>
2022-08-29 13:57:15 +0530

    Time: 269 ms (39.09%), Space: 10.1 MB (96.83%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -1,44 +1,23 @@
-#include <unordered_map>
-#include <iostream> 
-#include <vector>
-
-using std::unordered_map;
-using std::vector;
-
-/*
-target = 9
-check a number minus by target
-agar wo mere list my hay to well and good
-kya negative karne se zero aya 
-nahi => remaining ke lie next se minus karo
-agar tb bhi nhi aya to 
-next wla first point ho jayga
-
-*/
-class Solution 
-{
+class Solution{
 public:
-    vector<int> twoSum(vector<int>& nums, int target) 
-    {
-       int n = nums.size();
-        vector<int>ans;
-        
-        unordered_map<int, int> mp;
-        
-        for(int i=0; i<n; i++){
-            if(mp.find(target-nums[i]) != mp.end()){ //agar humko target milega to push karenge 
-                
-                ans.push_back(mp[target-nums[i]]);//target ka value
-                
-                ans.push_back(i);//target ka index
-                return ans;
-                
+vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> index;
+        int size=nums.size();
+        for(int i=0;i<size;i++)
+        {
+            int k=target-nums[i];
+            for(int j=i+1;j<size;j++)
+            {
+                if(nums[j]==k)
+                {
+                    index.push_back(i);
+                    index.push_back(j);
+                    break;
+                }
             }
-            else
-                mp[nums[i]] = i;
+            if(index.size()==2)
+                break;
         }
-        return ans;//kuch nahi mila to ans return
-        
+        return index;
     }
-  
 };
