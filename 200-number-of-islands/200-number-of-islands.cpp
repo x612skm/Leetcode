803f06135afb64ca61b2f4f4b8dac75506851a4a
Soubhik Kumar Mitra <59209034+x612skm@users.noreply.github.com>
2022-08-29 13:59:58 +0530

    Time: 49 ms (64.88%), Space: 12.3 MB (83.10%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,36 +1,27 @@
 class Solution {
 public:
     int numIslands(vector<vector<char>>& grid) {
-        int m = grid.size();
-        int n = m ? grid[0].size() : 0;
-        int island = 0;
-        
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                if(grid[i][j] == '1')
-                {
-                    island++;
-                    eraseIsland(grid,i,j);
+        int m = grid.size(), n = m ? grid[0].size() : 0, islands = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == '1') {
+                    islands++;
+                    eraseIslands(grid, i, j);
                 }
             }
         }
-        return island;
+        return islands;
     }
-    
 private:
-    void eraseIsland(vector<vector<char>>& grid, int i, int j){
-        int m = grid.size();
-        int n = grid[0].size();
-        
-        if(i==m || i < 0 || j == n || j < 0 || grid[i][j] == '0')
+    void eraseIslands(vector<vector<char>>& grid, int i, int j) {
+        int m = grid.size(), n = grid[0].size();
+        if (i < 0 || i == m || j < 0 || j == n || grid[i][j] == '0') {
             return;
-        
+        }
         grid[i][j] = '0';
-        
-        eraseIsland(grid, i-1, j);
-        eraseIsland(grid, i+1, j);
-        eraseIsland(grid, i, j-1);
-        eraseIsland(grid, i, j+1);
-
+        eraseIslands(grid, i - 1, j);
+        eraseIslands(grid, i + 1, j);
+        eraseIslands(grid, i, j - 1);
+        eraseIslands(grid, i, j + 1);
     }
 };
