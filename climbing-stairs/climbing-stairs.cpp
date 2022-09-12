fb6cfb53ea3dd29a411fd00cf698821ce8523bc8
Soubhik Kumar Mitra <59209034+x612skm@users.noreply.github.com>
2022-09-12 21:09:21 +0530

    Time: 3 ms, Memory: 6.2 MB - LeetHub

---
+++ climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n<=3) return n;
+        int * arr = new int[n+1];
+        
+        arr[0] = 0;
+        arr[1] = 1;
+        arr[2] = 2;
+        arr[3] = 3;
+        
+        for(int i=4; i<=n; i++){
+            arr[i] = arr[i-1] + arr[i-2];
+        }
+        
+        int res = arr[n];
+        delete []arr;
+        return res;
+    }
+};
