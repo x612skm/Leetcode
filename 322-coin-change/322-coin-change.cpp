260e7975642e24958f3006345f81d021be1393b4
Soubhik Kumar Mitra <59209034+x612skm@users.noreply.github.com>
2022-09-15 22:17:05 +0530

    Time: 184 ms (33.59%), Space: 10.4 MB (91.85%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -6,16 +6,16 @@ public:
         for(int i=0; i<arrSize + 1; i++){
             for(int j=0; j<amount+1; j++){
                 if(i ==0 || j ==0)
-                    dp[i][j] = (j==0) ? 0 : INT_MAX - 1; // to avoid the overflow
+                    dp[i][j] = (j==0) ? 0 : INT_MAX -1; // to avoid the overflow
             }
         }
         
         for(int i=1; i<arrSize+1; i++){
             for(int j=0; j<amount+1; j++){
                 if(coins[i-1] <= j)
-                    dp[i][j] = min(0+dp[i-1][j], 1+dp[i][j-coins[i-1]]);
+                    dp[i][j] = min(dp[i-1][j], 1+dp[i][j-coins[i-1]]);
                 else
-                    dp[i][j] = 0 + dp[i-1][j];
+                    dp[i][j] = dp[i-1][j];
             }
         }
         return dp[arrSize][amount];
