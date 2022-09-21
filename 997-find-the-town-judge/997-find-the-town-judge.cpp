f4ef73c1291ebd329ba6f591f86d05cd56dc0327
Soubhik Kumar Mitra <59209034+x612skm@users.noreply.github.com>
2022-09-22 02:12:12 +0530

    Time: 431 ms (13.07%), Space: 62.7 MB (51.86%) - LeetHub

--- 997-find-the-town-judge/997-find-the-town-judge.cpp
+++ 997-find-the-town-judge/997-find-the-town-judge.cpp
@@ -1,13 +1,22 @@
 class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
-        vector<int> trusted(n+1,0);
-        for(auto person : trust){
-            trusted[person[0]]--;
-            trusted[person[1]]++;
+        //using map data structure
+        if(n==1) return 1;
+        map<int,int>first;
+        map<int,int>second;
+        
+        for(int i=0; i< trust.size(); i++){
+            first[trust[i][0]]++;
         }
-        for(int i=1; i<=n; i++){
-            if(trusted[i] == n-1)
+        
+        for(int i=0; i<trust.size(); i++){
+            second[trust[i][1]]++;
+            
+        }
+        
+        for(int i=0; i<=n; i++){
+            if(first[i] == 0 and second[i] == n-1)
                 return i;
         }
         return -1;
