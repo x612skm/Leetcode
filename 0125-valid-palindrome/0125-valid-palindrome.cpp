b67718b2beace90d36037dd1304bc35d13af6ca5
Soubhik Kumar Mitra <59209034+x612skm@users.noreply.github.com>
2022-10-15 00:23:32 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (90.51%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -4,7 +4,6 @@ public:
         for(int i=0, j=s.size()-1; i<j; i++, j--){
             while(!isalnum(s[i]) and i<j) i++;
             while(!isalnum(s[j]) and i<j) j--;
-            
             if(tolower(s[i]) != tolower(s[j])) return false;
         }
         return true;
