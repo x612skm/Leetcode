9289e532dfa310effebb4820b0c6f3a6d109da23
Soubhik Kumar Mitra <59209034+x612skm@users.noreply.github.com>
2022-10-19 22:43:44 +0530

    Time: 144 ms (61.98%), Space: 10.6 MB (85.32%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -14,7 +14,7 @@ public:
         //since this is a take and again take conditon
         //we can take the coin again
         for(int i=1; i<value+1; i++){
-            for(int j=1; j<amount+1; j++){
+            for(int j=0; j<amount+1; j++){
                 if(coins[i-1] <= j)
                     dp[i][j] = min(dp[i-1][j],1+dp[i][j-coins[i-1]]);
                 else
