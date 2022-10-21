262815762b24f468027313bba3f0414551fad433
Soubhik Kumar Mitra <59209034+x612skm@users.noreply.github.com>
2022-10-21 14:47:29 +0530

    Time: 4 ms (93.58%), Space: 10.7 MB (95.95%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -0,0 +1,34 @@
+class Solution {
+private:
+    void combinationSum(vector<int>&candidates, int target, vector<vector<int>>&result, vector<int>&temp_storage, int index){
+        if(target == 0)
+        {
+            result.push_back(temp_storage);
+            return;
+        }
+        //there we check for the negative nuber and for the index not going out of bound
+        while(index < size(candidates) and target - candidates[index] >= 0){
+            temp_storage.push_back(candidates[index]);
+            //recurance for the next
+            combinationSum(candidates, target-candidates[index], result, temp_storage, index);
+            //increasing the index to check the next;
+            ++index;
+            //back track aagain keeping the numbver out
+            temp_storage.pop_back();
+        }
+        
+    }
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        //simple backtracking problem
+        sort(candidates.begin(), candidates.end());
+        //remove the duplicate if asked in the array else not given in thisquestion
+        //candidates.erase(unique(candidates.begin(), candidates.end(), candidates.end()));
+        
+        vector<vector<int>> result;
+        vector<int>temp_storage;
+        
+        combinationSum(candidates, target, result, temp_storage, 0);
+        return result;
+    }
+};
