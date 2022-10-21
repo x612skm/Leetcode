14acd19a79ae6e54e9d76c94ca6977829548883a
Soubhik Kumar Mitra <59209034+x612skm@users.noreply.github.com>
2022-10-21 18:45:23 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (97.61%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.cpp
+++ 0844-backspace-string-compare/0844-backspace-string-compare.cpp
@@ -1,46 +1,27 @@
 class Solution {
-// private:
-//     //tc = n
-//     string compare(string s){
-//         int count = 0;
-//         string ans = "";
-//         for(int i=s.size()-1; i>=0; --i){
-//             if(!isalnum(s[i]))
-//                 count++;
-//             else{
-//                 if(count == 0){
-//                     ans += s[i];
-//                     continue;
-//                 }
-//                 count--;
-//             }
-//         }
-//         reverse(ans.begin(), ans.end()); //O(n)
-//         return ans;
-//     }
-    //just checking the space complexity
-public:
-   bool backspaceCompare(string S, string T) {
-        int i = S.length() - 1, j = T.length() - 1, back;
-        while (true) {
-            back = 0;
-            while (i >= 0 && (back > 0 || S[i] == '#')) {
-                back += S[i] == '#' ? 1 : -1;
-                i--;
-            }
-            back = 0;
-            while (j >= 0 && (back > 0 || T[j] == '#')) {
-                back += T[j] == '#' ? 1 : -1;
-                j--;
-            }
-            if (i >= 0 && j >= 0 && S[i] == T[j]) {
-                i--;
-                j--;
-            } else {
-                break;
+private:
+    //tc = n
+    string compare(string s){
+        int count = 0;
+        string ans = "";
+        for(int i=s.size()-1; i>=0; --i){
+            if(!isalnum(s[i]))
+                count++;
+            else{
+                if(count == 0){
+                    ans += s[i];
+                    continue;
+                }
+                count--;
             }
         }
-        return i == -1 && j == -1;
+        reverse(ans.begin(), ans.end()); //O(n)
+        return ans;
     }
-
+public:
+    bool backspaceCompare(string s, string t) {
+        if(compare(s) == compare(t))
+            return true;
+        return false;
+    }
 };
