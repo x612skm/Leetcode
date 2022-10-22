f8c5952e965bef3fb17a8c9dfec775ce202224ea
Soubhik Kumar Mitra <59209034+x612skm@users.noreply.github.com>
2022-10-22 13:18:44 +0530

    Time: 5 ms (37.08%), Space: 6 MB (47.54%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -1,6 +1,11 @@
 class Solution {
 public:
     int hammingWeight(uint32_t n) {
-        return __builtin_popcountll(n);
+        int count = 0;
+        while(n>0){
+            count++;
+            n &= (n-1);
+        }
+        return count;
     }
 };
