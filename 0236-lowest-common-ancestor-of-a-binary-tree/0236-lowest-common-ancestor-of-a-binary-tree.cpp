d612cfc63cd09e7afc56e720563c0b33cd620212
Soubhik Kumar Mitra <59209034+x612skm@users.noreply.github.com>
2022-10-25 18:02:46 +0530

    Time: 43 ms (14.80%), Space: 14.3 MB (23.16%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -11,10 +11,13 @@ class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
         if(!root or root == p or root == q) return root;
+        //same as the lca of bst in this we dont have to check the weights
         TreeNode* leftsearch = lowestCommonAncestor(root->left,p,q);
         TreeNode* rightsearch = lowestCommonAncestor(root->right,p,q);
+        
         if(leftsearch and rightsearch) return root;
         if(leftsearch) return leftsearch;
+        
         return rightsearch;
     }
 };
