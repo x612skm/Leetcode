c4feb623e6c89183654b0d493b917bbd02f03ec6
Soubhik Kumar Mitra <59209034+x612skm@users.noreply.github.com>
2022-10-27 22:16:38 +0530

    Time: 58 ms (58.15%), Space: 12.3 MB (55.89%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,32 +1,27 @@
 class Solution {
 private:
-    void eraseIsland(vector<vector<char>>&grid, int i, int j){
-        //both i and j are matrix sizes
-        //checking the base or outline condtn for dfs
-        if(i<0 or j<0 or i==grid.size() or j==grid[0].size() or grid[i][j] == '0')
+    void erase(vector<vector<char>>& grid, int i, int j){
+        //cecking the edge case conditons
+        if(i>=grid.size() or i < 0 or j>=grid[0].size() or j<0 or grid[i][j] == '0')
             return;
         
         grid[i][j] = '0';
-        eraseIsland(grid, i-1, j);
-        eraseIsland(grid,i+1,j);
-        eraseIsland(grid,i,j-1);
-        eraseIsland(grid,i,j+1);
+        erase(grid, i+1, j);
+        erase(grid, i-1, j);
+        erase(grid, i, j+1);
+        erase(grid, i, j-1);
     }
 public:
     int numIslands(vector<vector<char>>& grid) {
-        //this question tends to be the graph coloring problem
-        //we will use dfs for it
-        //wherever we find 0 we make the adjacent to 0 
         int m = grid.size();
-        int n = m ? grid[0].size() : 0;
+        int n =m ? grid[0].size() : n;
         int island = 0;
-        //searching in the matrix
+        
         for(int i=0; i<m; i++){
-            for(int j=0; j<n;j++){
-                if(grid[i][j] == '1')
-                {
+            for(int j=0; j<n; j++){
+                if(grid[i][j] == '1'){
                     island++;
-                    eraseIsland(grid,i,j);
+                    erase(grid,i,j);
                 }
             }
         }
