cbee1611644963f67191f9c19ebee922510e8ff2
Soubhik Kumar Mitra <59209034+x612skm@users.noreply.github.com>
2022-11-02 20:17:08 +0530

    Time: 182 ms (8.63%), Space: 47.1 MB (5.76%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -8,31 +8,14 @@ public:
             return a[0] < b[0];
         });
         
-        vector<vector<int>>result;
-        vector<int> inter;
+        vector<vector<int>> result;
+        result.push_back(intervals[0]);
         
-        //inter.push_back(intervals[0][0]);
-        inter = intervals[0];
-        //[1,4] => inter
-        //inter = intervals[0];
-        result.push_back(inter);
-        //inter.push_back(intervals[0]);
-        for(auto i = 1; i<intervals.size(); i++){
-            //4 => inter[1] 
-            if(inter[1] >= intervals[i][0]){
-                //inter.push_back(intervals[i+1][1]);
-                inter[1] = max(inter[1], intervals[i][1]);
-                result.pop_back();
-                result.push_back(inter);
-            }
-            //intervals
-            
-            else{
-                //inter.clear();
-                inter = intervals[i];
-                result.push_back(inter);
-            }
-            //result.push_back(inter);
+        for(int i=1; i<intervals.size(); i++){
+            if(result.back()[1] >= intervals[i][0])
+                result.back()[1] = max(result.back()[1], intervals[i][1]);
+            else
+                result.push_back(intervals[i]);
         }
         return result;
     }
