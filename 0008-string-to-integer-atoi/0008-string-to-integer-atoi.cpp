9b1bd1e4b1f2ee04c5200ba92287afc2262c8b79
Soubhik Kumar Mitra <59209034+x612skm@users.noreply.github.com>
2022-11-03 18:03:24 +0530

    Time: 4 ms (75.52%), Space: 7 MB (88.17%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,49 +1,41 @@
 class Solution {
+private:
+    bool isDigit(char ch){
+        return ch>='0' and ch<='9';
+    }
 public:
     int myAtoi(string s) {
-        
-        const int len = s.size();
-        
-        if(len == 0){
+        int len = s.size();
+        if(len == 0)
             return 0;
-        }
         
         int index = 0;
-        while(index < len && s[index] == ' '){
-            ++index;
-        }
-
-        if(index == len){
+        
+        while(index<len and s[index]==' ')
+            index++;
+        
+        if(index == len)
             return 0;
-        }
         
         char ch;
         bool isNegative = (ch = s[index]) == '-';
         
-        if(isNegative || ch == '+'){
+        if(isNegative or ch == '+')
             ++index;
-        }
-		
-        const int maxLimit = INT_MAX / 10;
+        
+        const int maxlimit = INT_MAX/10;
+        
         int result = 0;
-        while(index < len && isDigit(ch = s[index])){ 
-
-            int digit = ch - '0';
-
-            if(result > maxLimit || (result == maxLimit && digit > 7)){
+        
+        while(index < len and isDigit(ch = s[index])){
+            int digit = ch -'0';
+            
+            if(result > maxlimit or (result == maxlimit and digit > 7)){
                 return isNegative ? INT_MIN : INT_MAX;
             }
-
             result = (result * 10) + digit;
-            
             ++index;
         }
-        
-        return isNegative ? -result : result;
-    }
-    
-    private:
-    bool isDigit(char ch){
-        return ch >= '0' && ch <= '9';
+        return isNegative ? -result : result; 
     }
 };
