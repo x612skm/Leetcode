e36028dea0efdac5eb090a5774da1e2d4ee9bce1
Soubhik Kumar Mitra <59209034+x612skm@users.noreply.github.com>
2022-11-08 19:20:55 +0530

    Time: 353 ms (48.74%), Space: 54.7 MB (53.65%) - LeetHub

---
+++ 0310-minimum-height-trees/0310-minimum-height-trees.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    vector<int> findMinHeightTrees(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int> adj[numCourses];
+        vector<int> indegree(numCourses,0);
+        vector<int>ans;
+        //compute indegree for each node
+        for(auto p:prerequisites){
+            adj[p[1]].push_back(p[0]);
+            adj[p[0]].push_back(p[1]);
+            indegree[p[0]]++;
+            indegree[p[1]]++;
+        }
+        queue<int> q;
+        for(int i=0; i<numCourses; i++){
+            if(indegree[i] == 1){
+                q.push(i);
+                indegree[i]--;
+            }
+            
+        }
+        
+        while(!q.empty()){
+            ans.clear();
+            int s = q.size();
+            while(s--){
+            int curr = q.front();
+            q.pop();
+            ans.push_back(curr);
+            numCourses--;
+            
+            for(auto child : adj[curr]){
+                if(--indegree[child] == 1)
+                    q.push(child);
+            }
+        }
+        }
+        if(numCourses==1)
+            ans.push_back(0);
+        
+        return ans;
+    }
+};
