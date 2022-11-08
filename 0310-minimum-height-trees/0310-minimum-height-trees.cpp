c68ebaa83eefef629cb9cc63c76df62247b1c14b
Soubhik Kumar Mitra <59209034+x612skm@users.noreply.github.com>
2022-11-08 19:24:10 +0530

    Time: 158 ms (97.26%), Space: 54.6 MB (53.65%) - LeetHub

--- 0310-minimum-height-trees/0310-minimum-height-trees.cpp
+++ 0310-minimum-height-trees/0310-minimum-height-trees.cpp
@@ -11,29 +11,29 @@ public:
             indegree[p[0]]++;
             indegree[p[1]]++;
         }
+        //make a bfs call
         queue<int> q;
         for(int i=0; i<numCourses; i++){
             if(indegree[i] == 1){
                 q.push(i);
                 indegree[i]--;
             }
-            
         }
         
         while(!q.empty()){
             ans.clear();
             int s = q.size();
             while(s--){
-            int curr = q.front();
-            q.pop();
-            ans.push_back(curr);
-            numCourses--;
-            
-            for(auto child : adj[curr]){
-                if(--indegree[child] == 1)
-                    q.push(child);
+                int curr = q.front();
+                q.pop();
+                ans.push_back(curr);
+                numCourses--;
+
+                for(auto child : adj[curr]){
+                    if(--indegree[child] == 1)
+                        q.push(child);
+                }
             }
-        }
         }
         if(numCourses==1)
             ans.push_back(0);
