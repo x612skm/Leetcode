dc968985dd465d836ec7d1854e248e31ad766e1d
Soubhik Kumar Mitra <59209034+x612skm@users.noreply.github.com>
2022-11-08 23:02:36 +0530

    Time: 196 ms (39.07%), Space: 34.4 MB (70.99%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -5,10 +5,11 @@ public:
         int count = 0;
         for(auto& it:tasks){
             mp[it]++;
+            //cout<<mp[it];
             count = max(count, mp[it]);
         }
         int ans = (count - 1) * (n + 1);
-        
+        //cout<<ans;
         for(auto& it:mp) if(it.second == count) ans++;
         return max((int)tasks.size(), ans);
    }
