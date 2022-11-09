3f3d3321a937fd9fb55724e8b4adcc868aa376dd
Soubhik Kumar Mitra <59209034+x612skm@users.noreply.github.com>
2022-11-09 12:09:14 +0530

    Time: 283 ms (14.54%), Space: 44 MB (15.18%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -1,16 +1,39 @@
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
-         unordered_map<int, int>mp;
-        int count = 0;
-        for(auto& it:tasks){
-            mp[it]++;
-            //cout<<mp[it];
-            count = max(count, mp[it]);
+        unordered_map<char, int>mp;
+        //we will have the tasks frequenecy
+        for(auto& t : tasks){
+            mp[t]++;
         }
-        int ans = (count - 1) * (n + 1);
-        //cout<<ans;
-        for(auto& it:mp) if(it.second == count) ans++;
-        return max((int)tasks.size(), ans);
-   }
+        
+        priority_queue<int>pq;
+        for(auto& count : mp){
+            pq.push(count.second);//push the frequency inside
+        }
+        
+        int allTime = 0;
+        int cycle = n+1;//we have to add 1 everytime to the cycle
+        while(!pq.empty()){
+            int time = 0;
+            vector<int>temp;
+            for(int i=0; i<cycle; ++i){
+                if(!pq.empty()){
+                    temp.push_back(pq.top());
+                    pq.pop();
+                    time++;
+                }
+            }
+            for(auto cnt : temp)
+                if(--cnt)
+                    pq.push(cnt);
+            
+            if(!pq.empty())
+                allTime += cycle;
+            else
+                allTime += time;
+        }
+        
+        return allTime;
+    }
 };
