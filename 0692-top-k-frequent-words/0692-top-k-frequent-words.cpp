dc2f17e2120c219bf156f63098f46bd75f80d7b3
Soubhik Kumar Mitra <59209034+x612skm@users.noreply.github.com>
2022-11-12 01:17:03 +0530

    Time: 34 ms (28.80%), Space: 12.9 MB (16.35%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
@@ -2,10 +2,11 @@ class Solution {
 //making a constructor for comparator function
 private:
     struct comp{
+        //if freq not equal sort accoring to the freq or lexiographical sorting
         bool operator()(pair<int,string>&lhs, pair<int,string>&rhs){
             if(lhs.first != rhs.first)
-                return lhs.first < rhs.first;
-            return lhs.second > rhs.second;
+                return lhs.first < rhs.first;//if the frequency is not same then acc to freq
+            return lhs.second > rhs.second;//if the frequency is same then lexiographical
         }
     };
 public:
@@ -15,16 +16,17 @@ public:
         for(auto& word : words){
             ++mp[word];
         }
-        
+        //minheap becasue we have to use a comparator fn to all the items
         priority_queue<pair<int, string>, vector<pair<int, string>>, comp>pq;
         //inside the pq it will sort acc to the comparator rwritten
         for(auto& it:mp)
             pq.emplace(it.second, it.first);
         
         vector<string>res;
-        
+        //insert for the k elements template for the heap questions
         while(k-->0 and !pq.empty()){
             res.push_back(pq.top().second);
+            //first elements will be automatically sorted using lexiographical order
             pq.pop();
         }
         return res;
