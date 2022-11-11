2532c7dcdb73be740d2fcc544575d2edc2f4b338
Soubhik Kumar Mitra <59209034+x612skm@users.noreply.github.com>
2022-11-11 14:06:13 +0530

    Time: 60 ms (84.01%), Space: 17.5 MB (87.63%) - LeetHub

---
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.cpp
@@ -0,0 +1,48 @@
+class Solution {
+private:
+    void dfs(vector<vector<int>>&heights, vector<vector<bool>>&visited, int row, int col, int prevheight){
+        int m = heights.size();
+        int n= heights[0].size();
+        //checking the out of bound conditons
+        if(row < 0 or col < 0 or row >= m or col >= n or visited[row][col] or heights[row][col] < prevheight)
+            return;
+        
+        visited[row][col] = true;
+        
+        //check for the directions of the cells using the condiotns
+        dfs(heights, visited, row-1, col, heights[row][col]);
+        dfs(heights, visited, row+1, col, heights[row][col]);
+        dfs(heights, visited, row, col-1, heights[row][col]);
+        dfs(heights, visited, row, col+1, heights[row][col]);
+    }
+public:
+    vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
+        int row = heights.size();
+        int col = heights[0].size();
+        
+        vector<vector<bool>> pac(row, vector<bool>(col, false));
+        vector<vector<bool>> atl( row , vector<bool> (col, false));
+        
+        //there will be one dfs call for pacific and one for atlantic
+        //check for the adjacnet rows for the pacific and atlantic
+        for(int r = 0; r<row; ++r){
+            dfs(heights, pac, r, 0, heights[r][0]);
+            dfs(heights, atl, r, col-1, heights[r][col-1]);
+        }
+        //check for the adjacent cols for the pacific and atlantic
+        for(int c=0; c<col; ++c){
+            dfs(heights, pac, 0, c, heights[0][c]);
+            dfs(heights, atl, row-1, c, heights[row-1][c]);
+        }
+        //append the result which cells are true and satifies the condition
+       vector<vector<int>>result;
+        for(int r=0; r<row; ++r){
+            for(int c=0; c<col; ++c){
+                if(pac[r][c] and atl[r][c])
+                    result.push_back({r,c});
+            }
+        }
+        //return the result
+        return result;
+    }
+};
