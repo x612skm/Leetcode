3d46a405012ffdf8374ef819add335933e831871
Soubhik Kumar Mitra <59209034+x612skm@users.noreply.github.com>
2022-11-12 01:45:56 +0530

    Time: 10 ms (9.60%), Space: 7.6 MB (55.08%) - LeetHub

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.cpp
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.cpp
@@ -8,10 +8,16 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+//Runtime: 0 ms, faster than 100.00% of C++ online submissions for Swap Nodes in Pairs.
+//Memory Usage: 7.5 MB, less than 91.68% of C++ online submissions for Swap Nodes in Pairs.
+
 class Solution {
 public:
     ListNode* swapPairs(ListNode* head) {
         //calling recursion for help
+        //1,2,3,4 => 2 = curr => calling recursion to handle for the rest case
+        //attchng 2 -> 1 rec[4->3]
+        //2->1->4->3
         if(head==nullptr or head->next==nullptr)
             return head;
         ListNode* curr = head->next;
