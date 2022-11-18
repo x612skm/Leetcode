76bb390c69a605f20f4ead0d0b23aae5ebc8f1fe
Soubhik Kumar Mitra <59209034+x612skm@users.noreply.github.com>
2022-11-18 22:36:07 +0530

    Time: 24 ms (48.76%), Space: 7 MB (53.00%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -5,9 +5,7 @@ public:
         int maxcnt = 0;
         int cnt = 0;
         for(auto i=0; i<s.size(); i++){
-            //mp[s[i]]++;
             maxcnt = max(maxcnt, ++mp[s[i]]);
-            //ut<<maxcnt<<" ";
             if(cnt - maxcnt < k){
                 cnt++; 
             }
