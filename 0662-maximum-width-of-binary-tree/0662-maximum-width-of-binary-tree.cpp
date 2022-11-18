e58d0fb95d44efc2dc24c0db6b1a2f7c709f3b22
Soubhik Kumar Mitra <59209034+x612skm@users.noreply.github.com>
2022-11-18 16:51:47 +0530

    Time: 20 ms (39.99%), Space: 17.3 MB (62.63%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
@@ -16,32 +16,27 @@ public:
         if(!root) return 0;
         queue<pair<TreeNode*, int>>q;
         
-        q.push({root,1});
+        q.push({root,0});
         
         int ans = 0;
         
         while(!q.empty()){
             int size = q.size();
-            // if(size == 1){
-            //     q.push({q.front().first, 1});
-            //     q.pop();
-            // }
-            //leftnode - rightnode + 1 is the maxdistance 
-            //we have to calculate the distance
+            
             int start  =q.front().second;
             int end = q.back().second;
             ans = max(end- start + 1, ans);
             
             while(size-- > 0){
                 auto node = q.front().first;
-                auto idx =  q.front().second- start;
+                auto idx =  q.front().second- start;//to avoid the overflow considering it a skew tree
                 //auto [node, idx] = q.front();
                 q.pop();
                 
                 if(node -> left)
-                    q.push({node->left, (long long)2 * idx});
+                    q.push({node->left, (long long)2 * idx + 1});
                 if(node -> right)
-                    q.push({node->right, (long long)2 * idx+ 1});
+                    q.push({node->right, (long long)2 * idx+ 2});
             }
         }
         return ans;
