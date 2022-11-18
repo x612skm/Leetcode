7e22ea17b85f328ba70d9cade5cbf1e431e3509c
Soubhik Kumar Mitra <59209034+x612skm@users.noreply.github.com>
2022-11-18 22:35:46 +0530

    Time: 24 ms (48.76%), Space: 7.1 MB (12.47%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -3,19 +3,17 @@ public:
     int characterReplacement(string s, int k) {
         unordered_map<char, int>mp;
         int maxcnt = 0;
-        int mincnt = 0;
         int cnt = 0;
         for(auto i=0; i<s.size(); i++){
             //mp[s[i]]++;
             maxcnt = max(maxcnt, ++mp[s[i]]);
             //ut<<maxcnt<<" ";
             if(cnt - maxcnt < k){
-                cnt++; cout<<cnt<<" ";
+                cnt++; 
             }
             else
                 mp[s[i-cnt]]--;
         }
-       
         return cnt;
     }
 };
