c0b7728e72d1e16479cd8f06547af4e8530cc4ae
Soubhik Kumar Mitra <59209034+x612skm@users.noreply.github.com>
2022-11-21 16:22:28 +0530

    Time: 232 ms (56.18%), Space: 47.3 MB (46.72%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -5,14 +5,11 @@ public:
         for(int i=0; i<k; i++){
             pq.push(nums[i]);
         }
-        
         for(int i=k; i<nums.size(); i++){
-            
             if(nums[i] > pq.top()){
                 pq.pop();
                 pq.push(nums[i]);
             }
-            
         }
         return pq.top();
     }
