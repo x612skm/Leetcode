e6ac1683b94192ea97465c3578e2b0c8d1370a8e
Soubhik Kumar Mitra <59209034+x612skm@users.noreply.github.com>
2022-11-22 23:07:34 +0530

    Time: 43 ms (8.31%), Space: 11 MB (87.78%) - LeetHub

--- 0179-largest-number/0179-largest-number.cpp
+++ 0179-largest-number/0179-largest-number.cpp
@@ -1,15 +1,15 @@
 class Solution {
-    struct comp {
+    struct comp{
         bool operator() (int a, int b) {
             string comb1 = to_string(a) + to_string(b);
             string comb2 = to_string(b) + to_string(a);
             return comb1 > comb2;
         }
-    }compare;
+    };
 public:
      
     string largestNumber(vector<int>& nums) {
-        sort(nums.begin(), nums.end(), compare);
+        sort(nums.begin(), nums.end(), comp());
         
         if(nums[0] == 0) return "0";
         
