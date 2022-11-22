bcf02f672b92da51c865d4c1cb680f16ea7fc9f4
Soubhik Kumar Mitra <59209034+x612skm@users.noreply.github.com>
2022-11-22 23:05:45 +0530

    Time: 39 ms (10.96%), Space: 11.1 MB (70.95%) - LeetHub

---
+++ 0179-largest-number/0179-largest-number.cpp
@@ -0,0 +1,23 @@
+class Solution {
+    struct comp {
+        bool operator() (int a, int b) {
+            string comb1 = to_string(a) + to_string(b);
+            string comb2 = to_string(b) + to_string(a);
+            return comb1 > comb2;
+        }
+    }compare;
+public:
+     
+    string largestNumber(vector<int>& nums) {
+        sort(nums.begin(), nums.end(), compare);
+        
+        if(nums[0] == 0) return "0";
+        
+        string res = "";
+        
+        for(auto num : nums){
+            res += to_string(num);
+        }
+        return res;
+    }
+};
