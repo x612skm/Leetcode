04e7b1f50aa051a64ebcdce24c348e05f60f2112
Soubhik Kumar Mitra <59209034+x612skm@users.noreply.github.com>
2022-11-22 19:55:39 +0530

    Time: 3 ms (53.56%), Space: 5.9 MB (81.37%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if(n == 0)
+            return 1;
+        if(x == 1)
+            return x;
+        if(n<0){
+            n = abs(n);
+            x = 1/x;
+        }
+        return (n%2 == 0) ? myPow(x*x, n/2) : x*myPow(x*x, n/2);
+    }
+};
