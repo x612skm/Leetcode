de4d09e0f77c4336b26bfc11b94eb29e1d59aaf7
Soubhik Kumar Mitra <59209034+x612skm@users.noreply.github.com>
2022-11-23 12:17:05 +0530

    Time: 41 ms (11.31%), Space: 13.2 MB (82.66%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,26 +1,19 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        //checking for the first row and col for the zero
         bool isColzero = false;
         bool isRowzero = false;
-        
-        //checking for the coloumn zeroth
+        //check for the zero col
         for(int i=0; i<matrix.size(); i++){
-            if(matrix[i][0] == 0){
-                isColzero = true;
-                break;
-            }
-        }
-        //checking for the row zeroth
-        for(int j=0; j<matrix[0].size(); j++){
-            if(matrix[0][j] == 0){
-                isRowzero = true;
-                break;
-            }
+           if(matrix[i][0] == 0)
+               isColzero = true;
         }
         
-        //checking for the first row and the col except filling the zero
+        for(int i=0; i<matrix[0].size(); i++){
+            if(matrix[0][i] == 0)
+                isRowzero = true;
+        }
+        
         for(int i=1; i<matrix.size(); i++){
             for(int j=1; j<matrix[0].size(); j++){
                 if(matrix[i][j] == 0){
@@ -30,24 +23,22 @@ public:
             }
         }
         
-        //filling the rest of the zeros
         for(int i=1; i<matrix.size(); i++){
             for(int j=1; j<matrix[0].size(); j++){
-                if(matrix[i][0] == 0 or matrix[0][j] == 0){
+                if(matrix[i][0] == 0 or matrix[0][j] == 0)
                     matrix[i][j] = 0;
-                }
+            }
+        }
+        if(isColzero){
+            for(int i=0; i<matrix.size(); i++){
+                matrix[i][0] = 0; 
             }
         }
         
-        //chking for the zerorow and zerocol for the first row and col
-        if(isColzero){
-            for(int i=0; i<matrix.size(); i++)
-                matrix[i][0] = 0;
-        }
-        
         if(isRowzero){
-            for(int i=0; i<matrix[0].size(); i++)
+            for(int i=0; i<matrix[0].size(); i++){
                 matrix[0][i] = 0;
+            }
         }
     }
 };
