b9c2fa94d6122542b462707b02a83680b0716688
Soubhik Kumar Mitra <59209034+x612skm@users.noreply.github.com>
2022-11-26 00:35:50 +0530

    Time: 7 ms (55.94%), Space: 10.2 MB (22.39%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -4,9 +4,10 @@ public:
         int start = 0; int end = nums.size()-1;
         
         while(start < end){
-            
+            //base case
             if(nums[start] < nums[end])
                 return nums[start];
+            //binary search
             int mid = start + end >> 1;
             if(nums[mid] >= nums[start])
                 start = mid + 1;
