8d99e14eeeb336827e54fed801c7c8ea377af880
Soubhik Kumar Mitra <59209034+x612skm@users.noreply.github.com>
2022-11-28 21:52:48 +0530

    Time: 29 ms (58.07%), Space: 8.1 MB (31.24%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -5,18 +5,21 @@ public:
         int counter = t.size();
         int begin = 0; int end = 0, d = INT_MAX, head = 0;
         
+        //make it into the map
         for(auto c:t){
             mp[c]++;
         }
-        
+        //ADOBECODEBANC t=ABC
         while(end < s.size()){
             if(mp[s[end++]]-- > 0){
                 counter--;
             }
+            
             while(counter == 0){
-                if(end - begin < d)
-                    d= end - (head = begin);
-                
+                if(end - begin < d){
+                    head = begin;
+                    d= end - head;
+                }
                 if(mp[s[begin++]]++ == 0)
                     counter++;
             }
