f5b027d11152e02577ff90296533236df312e263
Soubhik Kumar Mitra <59209034+x612skm@users.noreply.github.com>
2022-12-01 21:59:43 +0530

    Time: 29 ms (65.44%), Space: 19.9 MB (61.98%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,15 +1,10 @@
 class Solution {
 public:
-    //kabhi kabhi man nahi karega magar hausla nahi harna hai
-    //karte rehna hai
     int trap(vector<int>& height) {
-        int n = height.size();
-        int left = 1; int right = n-2;
+        int left = 1; int right = height.size()-2;
+        int maxleft = height[0]; int maxright = height[height.size()-1];
         int ans = 0;
-        int maxleft = height[0]; int maxright = height[n-1];
-        
         while(left <= right){
-            //till then the right is not equal to the left
             if(maxleft < maxright){
                 if(maxleft < height[left])
                     maxleft = height[left];
@@ -21,8 +16,8 @@ public:
                 if(maxright < height[right])
                     maxright = height[right];
                 else
-                    ans+=maxright - height[right];
-                --right;
+                    ans+= maxright - height[right];
+                right--;
             }
         }
         return ans;
