3e22c3f7447239742e06a222c7038d9bb692c46f
Soubhik Kumar Mitra <59209034+x612skm@users.noreply.github.com>
2022-12-01 16:41:04 +0530

    Time: 67 ms (16.08%), Space: 23.9 MB (87.61%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        //we have to check 3 cases 
-        // 1.zero count 
-        // 2.product
-        // 3.division by nums[i];
+        int product = 1;
+        int countzero = count(nums.begin(), nums.end(), 0);
+        if(countzero > 1) return vector<int> (nums.size());
         
-        int product = 1; 
-        int zeroCount = count(nums.begin(), nums.end(), 0);
-        if(zeroCount > 1) return vector<int> (size(nums));
-        for(auto& n : nums){
-            if(n)
-                product *= n;
+        for(auto& it : nums){
+            if(it)
+                product *= it;
         }
         
-        for(auto& n : nums){
-            if(zeroCount) //if there's only one zero
-                n = n ? 0 : product;
-            else
-                n = product/n;
+        for(auto& it: nums){
+            if(countzero)
+                it = it ? 0 : product;
+            else{
+                it = product/it;
+            }
         }
         return nums;
     }
