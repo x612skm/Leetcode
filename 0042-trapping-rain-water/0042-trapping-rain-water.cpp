fcb2eda8659baa976b1c67c151cc8e5548291a9a
Soubhik Kumar Mitra <59209034+x612skm@users.noreply.github.com>
2022-12-01 22:03:18 +0530

    Time: 39 ms (39.53%), Space: 19.6 MB (97.18%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,23 +1,26 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
+        //marking the left and the right
         int left = 1; int right = height.size()-2;
+        //makeing two pointers 
         int maxleft = height[0]; int maxright = height[height.size()-1];
         int ans = 0;
+        //left == right because we have to caluclate the last cell also
         while(left <= right){
             if(maxleft < maxright){
                 if(maxleft < height[left])
                     maxleft = height[left];
-                else
-                    ans += maxleft - height[left];
-                left++;
+                else //if it is less then calculate the difference 
+                    ans += maxleft - height[left];//the diffrence is the req result
+                left++;//if left < right
             }
             else{
                 if(maxright < height[right])
                     maxright = height[right];
                 else
                     ans+= maxright - height[right];
-                right--;
+                right--;//if right less than left
             }
         }
         return ans;
