fc50854a70d298b439ff28ef5690a79c2d50a7d9
Soubhik Kumar Mitra <59209034+x612skm@users.noreply.github.com>
2022-12-03 21:23:41 +0530

    Time: 28 ms (34.10%), Space: 7.9 MB (81.30%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -14,7 +14,6 @@ public:
         while(fast and fast->next){
             slow = slow->next;
             fast = fast->next->next;
-            
             if(slow == fast)
                 return true;
         }
