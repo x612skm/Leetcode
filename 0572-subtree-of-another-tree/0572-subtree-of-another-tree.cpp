efe13744ca118dd4046d19e0dc02e81d3874230a
Soubhik Kumar Mitra <59209034+x612skm@users.noreply.github.com>
2022-12-07 21:18:23 +0530

    Time: 67 ms (10.90%), Space: 29 MB (17.54%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.cpp
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.cpp
@@ -10,17 +10,16 @@
  * };
  */
 class Solution {
-public:
-    //taking this from same tree
-    bool compare(TreeNode* root1 , TreeNode*root2){
-        if(root1 == nullptr or root2==nullptr) return root1 == root2;
-        //if(root1->val == root2->val) return true;
+private:
+    bool compare(TreeNode* root1, TreeNode* root2){
+        if(root1 == nullptr or root2 == nullptr) return root1 == root2;
         return root1->val == root2->val and compare(root1->left, root2->left) and compare(root1->right, root2->right);
     }
+public:
     bool isSubtree(TreeNode* root, TreeNode* subRoot) {
-        if(root == nullptr) return false;
-        if(compare(root,subRoot)) return true;
-        //just a little change we check either of the side of the tree for subtree
-        return isSubtree(root->left, subRoot) or isSubtree(root->right, subRoot);
+        if(!root) return false;
+        if(compare(root, subRoot)) return true;
+        return isSubtree(root->left,subRoot) or isSubtree(root->right,subRoot);
+        
     }
 };
