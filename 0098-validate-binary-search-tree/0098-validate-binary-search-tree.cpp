bb6f8e663bac19a49cf0cb9633f223907127c0a3
Soubhik Kumar Mitra <59209034+x612skm@users.noreply.github.com>
2022-12-07 22:32:20 +0530

    Time: 11 ms (94.63%), Space: 21.6 MB (89.65%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -10,14 +10,16 @@
  * };
  */
 class Solution {
-public:
+private:
     bool checker(TreeNode* root, long min, long max){
-        if(root == nullptr) return true;
-        //that means ekdam right and ekdam left nahi hai root
-        if(root->val >= max or root->val <= min) return false;
-        return checker(root->left,min, root->val) and checker(root->right,root->val,max);
+        if(!root) return true;
+        if(root->val <= min or root->val >= max)
+            return false;
+        return checker(root->left, min, root->val) and checker(root->right, root->val,max);
     }
+public:
     bool isValidBST(TreeNode* root) {
         return checker(root, LONG_MIN, LONG_MAX);
+        
     }
 };
