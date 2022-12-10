d4a06a997864badba0ada0851932cff177ace98c
Soubhik Kumar Mitra <59209034+x612skm@users.noreply.github.com>
2022-12-10 22:29:48 +0530

    Time: 28 ms (40.15%), Space: 7.1 MB (52.73%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,22 +1,15 @@
-/*
-Runtime: 4 ms, faster than 98.04% of C++ online submissions for Longest Repeating Character Replacement.
-Memory Usage: 7 MB, less than 53.00% of C++ online submissions for Longest Repeating Character Replacement.
-*/
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        //making a char map
         unordered_map<char, int>mp;
         int maxcnt = 0;
         int cnt = 0;
         for(auto i=0; i<s.size(); i++){
             int freq = ++mp[s[i]];
-            maxcnt = max(maxcnt, freq); 
-            if(cnt - maxcnt < k){
-                cnt++; 
-            }
-            else
-                mp[s[i-cnt]]--;
+            maxcnt = max(maxcnt, freq);
+            
+            if(cnt - maxcnt < k) cnt++;
+            else mp[s[i-cnt]]--;
         }
         return cnt;
     }
