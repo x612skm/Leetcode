17eba8872e06d720f41f083f9c88049afa9a8200
Soubhik Kumar Mitra <59209034+x612skm@users.noreply.github.com>
2022-12-14 20:41:45 +0530

    Time: 8 ms (48.30%), Space: 13.7 MB (43.99%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,27 +1,21 @@
 class Solution {
 private:
-    void generate(vector<string>&ans, string p, int n, int m){
-        if(n ==0 and m ==0){
+    void generate(vector<string>& ans, string p, int n, int m){
+        //using backtracking template
+        if(n==0 and m==0){
             ans.push_back(p);
             return;
         }
         
-        if(n > 0)
-            generate(ans, p + '(', n-1, m+1);
-        if(m > 0)
-            generate(ans, p + ')', n, m-1);
-        
+        if(n>0) generate(ans, p + '(', n-1, m+1);
+        if(m>0) generate(ans, p + ')', n, m-1);
     }
 public:
     vector<string> generateParenthesis(int n) {
-        //all pair => backtrack we can have the 
-        /*
-        1 => () | 2 => (()) , ()()
-        */
-        
+        //making the return type
         vector<string> ans;
         string p = "";
-        generate(ans,p,n,0);
+        generate(ans, p, n, 0);
         return ans;
     }
 };
