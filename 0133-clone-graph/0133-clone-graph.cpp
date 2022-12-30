c0fa6deafcc6d8ef8e75eaeb8ad891da3909ea0c
Soubhik Kumar Mitra <59209034+x612skm@users.noreply.github.com>
2022-12-30 20:37:43 +0530

    Time: 11 ms (55.82%), Space: 8.9 MB (50.69%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.cpp
+++ 0133-clone-graph/0133-clone-graph.cpp
@@ -12,8 +12,6 @@ public:
         val = _val;
         neighbors = vector<Node*>();
     }
-    creating a new node will be
-    Node* copy = new Node(node->val, {}); //having no neighbors
     Node(int _val, vector<Node*> _neighbors) {
         val = _val;
         neighbors = _neighbors;
@@ -24,34 +22,23 @@ public:
 class Solution {
 public:
     Node* cloneGraph(Node* node) {
-        //using this as the bfs because we have to search for the nodes and the relations
-        //at the same time
-        //there is a think of the bfs
-        unordered_map<Node*, Node*> mp;
-        if(node == nullptr) return nullptr;
-        //makign a new copy of the node
-        Node* copy = new Node(node->val, {});
-        //since using bfs making a queue for the storage for the curr nodes
-        queue<Node*> storage;
+        if(!node)
+            return nullptr;
+        return dfs(node);
+    }
+private:
+    unordered_map<Node*, Node*> mp;
+    Node* dfs(Node* node){
+        if(mp.find(node) != mp.end())
+           return mp[node];
         
-        mp[node] = copy;
-        storage.push(node);
+        //create the first node
+        Node* neew = new Node(node->val);
+        mp[node] = neew;
         
-        while(!storage.empty()){
-            Node* curr = storage.front();
-            storage.pop();
-            
-            for(Node* neigh : curr -> neighbors){
-                if(mp.find(neigh) == mp.end()){
-                    mp[neigh] = new Node(neigh->val, {});
-                    storage.push(neigh);
-                }
-                //this will create the nodes for this
-                //neigh stands for the curr k neighbours
-                //mp[1] -> neighbors.push_back(mp[2,4]);
-                mp[curr] -> neighbors.push_back(mp[neigh]);
-            }
+        for(auto neigh : node->neighbors){
+            neew->neighbors.push_back(dfs(neigh));
         }
-        return copy;
+        return neew;
     }
 };
