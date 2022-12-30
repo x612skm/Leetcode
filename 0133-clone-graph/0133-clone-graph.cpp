8b82d4e3b65f24b21af78463d11315ec38335dc1
Soubhik Kumar Mitra <59209034+x612skm@users.noreply.github.com>
2022-12-30 20:38:48 +0530

    Time: 7 ms (83.83%), Space: 8.7 MB (72.93%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.cpp
+++ 0133-clone-graph/0133-clone-graph.cpp
@@ -34,9 +34,11 @@ private:
         
         //create the first node
         Node* neew = new Node(node->val);
-        mp[node] = neew;
+        mp[node] = neew;//mapping the first node
         
+        //traversing to the every node and calling dfs for the neightbours
         for(auto neigh : node->neighbors){
+            //it will create a new node everytime and map it with the node of graph
             neew->neighbors.push_back(dfs(neigh));
         }
         return neew;
