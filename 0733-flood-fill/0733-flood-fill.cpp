2a16cfe24836fb6e69a7875962a7b8ddd88160c0
Soubhik Kumar Mitra <59209034+x612skm@users.noreply.github.com>
2022-12-30 20:57:05 +0530

    Time: 15 ms (60.64%), Space: 13.8 MB (91.22%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,21 +1,18 @@
 class Solution {
-public:
-    void dfs(vector<vector<int>>& image, int sr, int sc,int val, int color){
-        if(sr < 0 or sc < 0 or sr >= image.size() or sc >= image[0].size() or image[sr][sc]!=val or image[sr][sc] == color){
+private:
+    void dfs(vector<vector<int>>& image,int val, int sr, int sc, int color){
+        if(sr<0 or sc < 0 or sr >= image.size() or sc >= image[0].size() or image[sr][sc] != val or image[sr][sc] == color)
             return;
-    }
         image[sr][sc] = color;
-        dfs(image, sr-1, sc, val, color);
-        dfs(image, sr+1, sc, val, color);
-        dfs(image, sr, sc-1, val, color);
-        dfs(image, sr, sc+1, val, color);
-        
-        
-        
+        dfs(image, val,sr-1, sc, color);
+        dfs(image, val,sr+1, sc, color);
+        dfs(image, val,sr, sc-1, color);
+        dfs(image, val,sr, sc+1, color);
     }
+public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         int val = image[sr][sc];
-        dfs(image, sr, sc, val, color);
+        dfs(image, val, sr, sc, color);
         return image;
     }
 };
