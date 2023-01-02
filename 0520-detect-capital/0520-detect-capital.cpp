3898026767c70c8f1e8acd00bd4cc8d6ddcd1014
Soubhik Kumar Mitra <59209034+x612skm@users.noreply.github.com>
2023-01-02 15:53:49 +0530

    Time: 5 ms (24.37%), Space: 6.1 MB (24.57%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.cpp
+++ 0520-detect-capital/0520-detect-capital.cpp
@@ -1,9 +1,11 @@
 class Solution {
 public:
     bool detectCapitalUse(string word) {
-        for(int i=1; i<word.size(); i++)
-            if(isupper(word[1]) != isupper(word[i]) or islower(word[0]) and isupper(word[i]))
+        for (int i = 1; i < word.size(); i++) {
+            if ((word[1] >= 'A' && word[1] <= 'Z') != (word[i] >= 'A' && word[i] <= 'Z') || (word[0] >= 'a' && word[0] <= 'z' && (word[i] >= 'A' && word[i] <= 'Z'))) {
                 return false;
+            }
+        }
         return true;
     }
 };
