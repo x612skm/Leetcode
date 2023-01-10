60f246f2b822ec17ee4f7c222497ee944215acfb
Soubhik Kumar Mitra <59209034+x612skm@users.noreply.github.com>
2023-01-11 00:05:17 +0530

    Time: 74 ms (92.01%), Space: 30.2 MB (53.56%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -1,27 +1,33 @@
-class Solution{
-public:
-int dfs(int i,int j,vector<vector<int>> &A){
-        if( i<0 || j<0 || i>=A.size() || j>=A[0].size() || A[i][j]==0)
-            return 0;
-        
-        A[i][j] = 0;
-        
-        return 1+dfs(i-1,j,A)+dfs(i+1,j,A)+dfs(i,j-1,A)+dfs(i,j+1,A);
-    }
-    
-    int numEnclaves(vector<vector<int>>& A) {
-        int all_one=0;
-        for(int i=0;i<A.size();i++){
-            for(int j=0;j<A[0].size();j++)
-                    all_one+=A[i][j];
+class Solution {
+private:
+    void dfs(vector<vector<int>>& a, int i, int j){
+        //make the 1 as 0 so that border island will vanish
+        //check for the out of bound conditons
+        if(i >= 0 && i <= a.size() - 1 && j >= 0 && j <= a[i].size() - 1 && a[i][j] == 1) {
+            a[i][j] = 0;
+            dfs(a, i + 1, j);
+            dfs(a, i - 1, j);
+            dfs(a, i, j + 1);
+            dfs(a, i, j - 1);
         }
-        int close_one = 0;
-        for(int i=0;i<A.size();i++){
-            for(int j=0;j<A[0].size();j++){
-                if((i==0 || j==0 || i==A.size()-1 || j==A[0].size()-1) && A[i][j]==1)
-                    close_one+= dfs(i,j,A);
+    }
+public:
+    int numEnclaves(vector<vector<int>>& grid) {
+        for(int i=0; i<grid.size(); i++){
+            for(int j=0; j<grid[0].size(); j++){
+                if(i==0 or j==0 or i == grid.size()-1 or j == grid[i].size()-1)
+                    dfs(grid, i, j);
             }
         }
-        return all_one - close_one;
+        
+        int result = 0;
+        
+        for(int i=0; i<grid.size(); i++){
+            for(int j=0; j<grid[0].size(); j++){
+                if(grid[i][j] == 1)
+                    result++;
+            }
+        }
+        return result;
     }
 };
