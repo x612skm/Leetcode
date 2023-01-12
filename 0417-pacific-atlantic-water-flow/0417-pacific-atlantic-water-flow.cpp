7520ee69b0d943231c09ea9b3702abfc1b1c6ceb
Soubhik Kumar Mitra <59209034+x612skm@users.noreply.github.com>
2023-01-12 18:11:41 +0530

    Time: 38 ms (96.75%), Space: 17.5 MB (76.43%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.cpp
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.cpp
@@ -1,7 +1,8 @@
 class Solution {
 private:
-    //same template used of FLood fill just add the visited component
-    void dfs(vector<vector<int>>&heights, vector<vector<bool>>&visited, int row, int col, int prevheight){
+    void dfs(vector<vector<int>>& heights, vector<vector<bool>>& visited, int row, int col, int prevheight){
+        
+        //checking for the out of bound condiotns
         int m = heights.size();
         int n= heights[0].size();
         //checking the out of bound conditons
@@ -10,7 +11,6 @@ private:
         
         visited[row][col] = true;
         
-        //check for the directions of the cells using the condiotns
         dfs(heights, visited, row-1, col, heights[row][col]);
         dfs(heights, visited, row+1, col, heights[row][col]);
         dfs(heights, visited, row, col-1, heights[row][col]);
@@ -18,32 +18,31 @@ private:
     }
 public:
     vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
+        //we have to search for both the layers of atlantic and pacific ocean
         int row = heights.size();
         int col = heights[0].size();
-        //checking the adjacent rows and cols to the pacfic and atlantic ocean
+        
         vector<vector<bool>> pac(row, vector<bool>(col, false));
         vector<vector<bool>> atl( row , vector<bool> (col, false));
-        
-        //there will be one dfs call for pacific and one for atlantic
-        //check for the adjacnet rows for the pacific and atlantic
-        for(int r = 0; r<row; ++r){
+        //rows for pacific and atlantic
+        for(int r=0; r<row; ++r){
             dfs(heights, pac, r, 0, heights[r][0]);
             dfs(heights, atl, r, col-1, heights[r][col-1]);
         }
-        //check for the adjacent cols for the pacific and atlantic
+        //col for pacific and atlantic
         for(int c=0; c<col; ++c){
             dfs(heights, pac, 0, c, heights[0][c]);
             dfs(heights, atl, row-1, c, heights[row-1][c]);
         }
-        //append the result which cells are true and satifies the condition
-       vector<vector<int>>result;
-        for(int r=0; r<row; ++r){
-            for(int c=0; c<col; ++c){
-                if(pac[r][c] and atl[r][c])
-                    result.push_back({r,c});
+        
+        vector<vector<int>> result;
+        
+        for(int i=0; i<row; ++i){
+            for(int j=0; j<col; ++j){
+                if(pac[i][j] and atl[i][j])
+                    result.push_back({i,j});
             }
         }
-        //return the result
         return result;
     }
 };
