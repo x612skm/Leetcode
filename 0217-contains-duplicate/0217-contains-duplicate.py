51c0ab68ad1f002e7d451eea56a49199a2c7714c
Soubhik Kumar Mitra <59209034+x612skm@users.noreply.github.com>
2023-03-17 00:17:14 +0530

    Time: 556 ms (75.57%), Space: 28.9 MB (42.17%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,3 +1,12 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        return len(nums) != len(set(nums))
+        #this code has a time complexity of O(n)
+        #return len(nums) != len(set(nums))
+        #to optimse the space complexity
+        
+        seen = set()
+        for n in nums:
+            if n in seen:
+                return True
+            seen.add(n)
+        return False
